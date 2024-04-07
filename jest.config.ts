2d13df4bb012455a203528de26fdc6b35a1441e5
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2024-04-07 09:14:32 +0200

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ export default {
 		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
 			"<rootDir>/mocks/fileMock.js",
 		"\\.(css|less)$": "<rootDir>/mocks/styleMock.js",
+		"\\\\.module\\\\.css$": "<rootDir>/mocks/styleMock.js",
 	},
 	collectCoverage: true,
 	coverageReporters: ["json", "html"],
