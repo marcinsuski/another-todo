9e454dff81c61c70e6e2b152dd698b4921875336
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2024-04-24 17:53:45 +0200

    refactor state management

--- src/store/todoListContext.tsx
+++ src/store/todoListContext.tsx
@@ -1,19 +1,78 @@
-import { createContext, ReactNode } from "react";
-import { useDispatch } from "react-redux";
+import { createContext, useState, useEffect, ReactNode, useMemo } from "react";
 import TodoList from "../classes/TodoList";
-
-export const TodoListContext = createContext<TodoList | null>(null);
+import TodoStore from "../classes/TodoStoreAdapter";
+import type { Todo, TodoListState } from "../types";
 
 type props = {
 	children: ReactNode;
 };
-export const TodoListProvider = ({ children }: props) => {
-	const dispatch = useDispatch();
 
-	const todoList = new TodoList(dispatch);
+export type TodoListContextType = {
+	todos: Todo[];
+	addTodo: (name: string) => void;
+	deleteTodo: (id: string) => void;
+	deleteAllTodos: () => void;
+	deleteCompletedTodos: () => void;
+	toggleTodo: (id: string) => void;
+};
+
+export const TodoListContext = createContext<TodoListContextType>({
+	todos: [],
+	addTodo: () => {},
+	deleteTodo: () => {},
+	deleteAllTodos: () => {},
+	deleteCompletedTodos: () => {},
+	toggleTodo: () => {},
+});
+
+export default function TodoListProvider({ children }: props) {
+	const initialStore: TodoListState = { todos: [] };
+	const store = useMemo(() => new TodoStore(initialStore), []);
+	const todoList = useMemo(() => new TodoList(store), [store]);
+
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	useEffect(() => {
+		setTodos(todoList.getTodos());
+	}, [todoList]);
+
+	const addTodo = (name: string) => {
+		todoList.addTodo(name);
+		setTodos(todoList.getTodos());
+	};
+
+	const deleteTodo = (id: string) => {
+		todoList.deleteTodo(id);
+		setTodos(todoList.getTodos());
+	};
+
+	const deleteAllTodos = () => {
+		todoList.deleteAllTodos();
+		setTodos(todoList.getTodos());
+	};
+
+	const deleteCompletedTodos = () => {
+		todoList.deleteCompletedTodos();
+		setTodos(todoList.getTodos());
+	};
+
+	const toggleTodo = (id: string) => {
+		todoList.toggleTodo(id);
+		setTodos(todoList.getTodos());
+	};
+
 	return (
-		<TodoListContext.Provider value={todoList}>
+		<TodoListContext.Provider
+			value={{
+				todos,
+				addTodo,
+				deleteTodo,
+				deleteAllTodos,
+				deleteCompletedTodos,
+				toggleTodo,
+			}}
+		>
 			{children}
 		</TodoListContext.Provider>
 	);
-};
+}
